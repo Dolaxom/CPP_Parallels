9b4f92243f5fef8369bbb44271229c71ff53cd0c
Nikolai <dolaxom4@gmail.com>
2023-07-08 20:40:09 +0300

    [main] Filling interface for Part 1 task

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
 #include "view/view.h"
 #include "model/graph/graph.h"
-
-void printAboba() {
-  std::cout << "Aboba aboba aboba\n";
-}
-
-void printFlex() {
-  std::cout << "Flex Flex Flex\n";
-}
+#include "model/graph/algorithms.h"
 
 int main() {
   s21::S21Graph graph = s21::S21Graph::S21LoadGraphFromFile("test.txt");
+  s21::S21Algorithms algorithm{};
 
   s21::S21View view("Parallels");
-  view.S21AddComponent("Print aboba", printAboba);
-  view.S21AddComponent("Print flex", printFlex);
+  view.S21AddComponent("Solving the Traveling Salesman problem with synchronous ant algorithm", [&algorithm]() { algorithm.S21SynchronousAntAlgorithm(); });
+  view.S21AddComponent("Solving the Traveling Salesman problem with asynchronous ant algorithm", [&algorithm]() { algorithm.S21AsynchronousAntAlgorithm(); });
+  view.S21AddComponent("Output the execution time of both algorithms", [&algorithm]() { algorithm.S21OutputTimes(); });
+
   view.S21Run();
 
   return 0;
