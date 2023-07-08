04e8186ccb735a1c3f682cf1cb17fdca63076cd3
Nikolai <dolaxom4@gmail.com>
2023-07-08 20:39:28 +0300

    [Algorithms][WIP] Adding base for graph methods

--- src/model/graph/algorithms.cpp
+++ src/model/graph/algorithms.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+
 #include "algorithms.h"
+
+namespace s21 {
+void S21Algorithms::S21SynchronousAntAlgorithm() {
+  synchronousTime_ = 1.25f;
+}
+
+void S21Algorithms::S21AsynchronousAntAlgorithm() {
+  asynchronousTime_ = 0.5f;
+}
+void S21Algorithms::S21OutputTimes() {
+  std::cout << "Synchronous time: " << synchronousTime_ << " s\n"
+      << "Asynchronous time: " << asynchronousTime_ << " s\n" <<
+      "difference in time: " << synchronousTime_ - asynchronousTime_ << " s.";
+
+}
+
+} // namespace s21
+
--- src/model/graph/algorithms.h
+++ src/model/graph/algorithms.h
@@ -1,6 +1,21 @@
 #ifndef SRC_MODEL_GRAPH_ALGORITHMS_H
 #define SRC_MODEL_GRAPH_ALGORITHMS_H
 
-class S21Algorithms {};
+namespace s21 {
+
+class S21Algorithms {
+ public:
+  S21Algorithms() = default;
+
+  void S21SynchronousAntAlgorithm();
+  void S21AsynchronousAntAlgorithm();
+  void S21OutputTimes();
+
+ private:
+  float synchronousTime_;
+  float asynchronousTime_;
+};
+
+}  // namespace s21
 
 #endif  // SRC_MODEL_GRAPH_ALGORITHMS_H
